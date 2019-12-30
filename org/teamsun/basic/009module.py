6ed047488b8ed4be82c0bc5076cb2cf7885e9a7d
Jacky <mousexjc@163.com>
2019-12-30 16:54:26 +0800

    package & module import

--- org/teamsun/basic/009module.py
+++ org/teamsun/basic/009module.py
@@ -3,10 +3,10 @@
 """
 import org.teamsun.pkgModule.myModule as myModule
 import org.teamsun.pkgModule as pkgModule
-from org.teamsun.pkgModule import myModule2
-import org.teamsun.pkgModule.myModule3
+from org.teamsun.pkgModule import myModule2, myModule3
+# import org.teamsun.pkgModule.myModule3
 from org.teamsun.pkgModule import *
 print(myModule.module1)
 print(pkgModule.pkgName)
 print(myModule2.module2)
-print(org.teamsun.pkgModule.myModule3.module3)
+print(myModule3.module3)
